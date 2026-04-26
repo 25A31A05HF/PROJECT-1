e7cd7c080031b5bef9d90e67ec3db45c628ee49a
25A31A05HF <hafeeja.sayyed@gmail.com>
2026-04-26 23:20:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     char op;
     double num1, num2;
 
-    printf("Enter an operator (+, -, *, /): ");
-    // The space before %c is crucial to consume any leftover newline character
-    scanf(" %c", &op); 
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &op);
 
     printf("Enter two operands: ");
     scanf("%lf %lf", &num1, &num2);
@@ -15,18 +15,29 @@ int main() {
         case '+':
             printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
             break;
+
         case '-':
             printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
             break;
+
         case '*':
             printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
             break;
+
         case '/':
             if (num2 != 0)
                 printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
             else
                 printf("Error! Division by zero is not allowed.\n");
             break;
+
+        case '%':
+            if (num2 != 0)
+                printf("%.2lf %% %.2lf = %.2lf\n", num1, num2, fmod(num1, num2));
+            else
+                printf("Error! Division by zero is not allowed.\n");
+            break;
+
         default:
             printf("Error! Operator is not correct.\n");
     }
